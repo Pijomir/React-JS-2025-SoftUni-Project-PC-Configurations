85b199672e558ab861401ca71c8fad138c710074
Anton <abbelkov@gmail.com>
2025-03-24 13:01:57 +0200

    replacing a with Link

--- client/src/components/MissingPage.jsx
+++ client/src/components/MissingPage.jsx
@@ -1,10 +1,12 @@
+import { Link } from "react-router";
+
 export default function MissingPage() {
    return (
       <div className="error-content">
          <h1>404</h1>
          <p>Oops! Page not found.</p>
          <p>Looks like it got lost somewhere on the internet.</p>
-         <a href="/" className="back-btn">Go Back to Home</a>
+         <Link to="/" className="back-btn">Go Back to Home</Link>
       </div>
    );
 }
